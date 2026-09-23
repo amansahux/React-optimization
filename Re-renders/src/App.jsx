f6fa2619d23dce7818fbe53b8776d7b84e203a0f
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-09-23 10:27:29 +0530

    reference props + useMemo hook

--- Re-renders/src/App.jsx
+++ Re-renders/src/App.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import Product from "./components/Product";
 
 const App = () => {
+  const product = useMemo(() => {
+    return { name: "Laptop", price: 500000 };
+  }, []);
   console.log("App Page re-rendering");
   const [count, setCount] = useState(0);
   return (
     <div>
-      <Product name="Laptop" />
+      <Product product={product} />
       <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   );
--- Re-renders/src/components/Product.jsx
+++ Re-renders/src/components/Product.jsx
@@ -1,13 +1,15 @@
-
 import { memo } from "react";
 
-const Product = memo( ({name}) => {
+const Product = memo(({ product }) => {
   console.log("Product Page re-rendering");
-  return <div>Product : {name}</div>;
-})
+  return (
+    <>
+      <div>Product Namee : {product.name}</div>;
+      <div>Product Price : {product.price}</div>;
+    </>
+  );
+});
 
 export default Product;
 
-// primitive values does not lead to re-renders
-
-
+// reference props lead to re-renders even same props accepts again we use useMemo when props are same where defining the reference props
